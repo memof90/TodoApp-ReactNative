96f945276cbef4664b97e1f27a2a72764de2adb7
memo figueredo <memof80@gmail.com>
2021-08-06 19:35:53 -0500

    picker created

--- src/Components/Forms.Component.js
+++ src/Components/Forms.Component.js
@@ -5,10 +5,18 @@ import tw from 'tailwind-react-native-classnames';
 // import DateTimePicker from '@react-native-community/datetimepicker';
 import { useNavigation } from '@react-navigation/native';
 
+// DROPDownPicker
+import DropDownPicker from 'react-native-dropdown-picker';
 
 // create a component
 const FormTask = () => {
   const [taskTitle, setTaskTitle] = useState('')
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState(null);
+  const [items, setItems] = useState([
+    {label: 'Apple', value: 'apple'},
+    {label: 'Banana', value: 'banana'}
+  ]);
 
   const navigation = useNavigation();
   // const [date, setDate] = useState(new Date(1598051730000));
@@ -32,13 +40,13 @@ const FormTask = () => {
 
 
     return (
-        <ScrollView>
+        <>
             <View style={styles.formRow}>
             <Text style={styles.formLabel}>Title</Text>
             </View>
             <View style={styles.formItem}>
             <TextInput 
-            style={tw `pl-3 pb-5 pt-4 bg-gray-200 rounded ml-4 w-80`}
+            style={tw `pl-3 pb-8 pt-4 bg-gray-200 rounded ml-4 w-80`}
               placeholder= "Desing Team Meeting"
               value={taskTitle}
               onChangeText={setTaskTitle}
@@ -49,7 +57,7 @@ const FormTask = () => {
             </View>
             <View style={styles.formItem}>
             <TextInput 
-            style={tw `pl-3 pb-5 pt-4 bg-gray-200 rounded ml-4 w-80`}
+            style={tw `pl-3 pb-8 pt-4 bg-gray-200 rounded ml-4 w-80`}
               placeholder= "2021-02-28"
             />
             </View>
@@ -72,7 +80,7 @@ const FormTask = () => {
             </View>
             <View style={styles.formItem}>
             <TextInput 
-            style={tw `pl-3 pb-5 pt-4 bg-gray-200 rounded ml-4 w-80`}
+            style={tw `pl-3 pb-8 pt-4 bg-gray-200 rounded ml-4 w-80`}
               placeholder= "10 minutes early"
             />
             </View>
@@ -80,9 +88,21 @@ const FormTask = () => {
             <Text style={styles.formLabel}>Repeat</Text>
             </View>
             <View style={styles.formItem}>
-            <TextInput 
+            {/* <TextInput 
             style={tw `pl-3 pb-5 pt-4 bg-gray-200 rounded ml-4 w-80`}
               placeholder= "10 minutes early"
+            /> */}
+            <DropDownPicker
+              style={tw.style('bg-gray-200 rounded ml-4 w-80', {borderWidth: 0})}
+              labelStyle={{
+              color: "#868686" 
+              }}
+              open={open}
+              value={value}
+              items={items}
+              setOpen={setOpen}
+              setValue={setValue}
+              setItems={setItems}
             />
             </View>
             <TouchableOpacity
@@ -93,7 +113,7 @@ const FormTask = () => {
                   <Text style={styles.btnText}> Create a Task </Text>
                 </View>
                </TouchableOpacity>
-        </ScrollView>
+        </>
     );
 };
 
@@ -130,7 +150,7 @@ const styles = StyleSheet.create({
     },
     touchableOpacity: {
         backgroundColor: '#23C774',
-        marginTop: 70,
+        marginTop: 80,
         marginLeft: 30
     },
     btnText: {
