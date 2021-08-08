25156b05f83a68534029821090ce9cd35d482df8
memo figueredo <memof80@gmail.com>
2021-08-07 19:15:51 -0500

    created form maybe better start Time and end Time labels and input

--- src/Components/Forms.Component.js
+++ src/Components/Forms.Component.js
@@ -1,6 +1,6 @@
 //import liraries
 import React, { useState } from 'react';
-import { View, Text, StyleSheet,ScrollView,TextInput, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet,TextInput, TouchableOpacity } from 'react-native';
 import tw from 'tailwind-react-native-classnames';
 // import DateTimePicker from '@react-native-community/datetimepicker';
 import { useNavigation } from '@react-navigation/native';
@@ -8,36 +8,57 @@ import { useNavigation } from '@react-navigation/native';
 // DROPDownPicker
 import DropDownPicker from 'react-native-dropdown-picker';
 
+import DateTimePicker from '@react-native-community/datetimepicker';
+import { Icon } from 'react-native-elements';
+
+
+
 // create a component
 const FormTask = () => {
   const [taskTitle, setTaskTitle] = useState('')
+  const [date, setDate] = useState(new Date());
+  const [time, setTime] = useState('06:00');
+  const [mode, setMode] = useState('date');
+  const [show, setShow] = useState(false);
   const [open, setOpen] = useState(false);
+  const [openRemind, setRemindOpen] = useState(false);
   const [value, setValue] = useState(null);
+  const [valueRemind, setValueRemind] = useState(null);
   const [items, setItems] = useState([
-    {label: 'Apple', value: 'apple'},
-    {label: 'Banana', value: 'banana'}
+    {label: 'Daily', value: 'Daily'},
+    {label: 'Weekly', value: 'Weekly'},
+    {label: 'Monthly', value: 'Monthly'},
+    {label: 'Quarterly', value: 'Quarterly'}
+  ]);
+  const [reminditems, setRemindiItems] = useState([
+    {label: '5 minutes early', value: '5m'},
+    {label: '10 minutes early', value: '10m'},
+    {label: '15 minutes early', value: '15m'},
+    {label: '20 minutes early', value: '20m'}
   ]);
 
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate || date;
+    setShow(Platform.OS === 'ios');
+    setDate(currentDate);
+  };
+
+  const showMode = (currentMode) => {
+    setShow(true);
+    setMode(currentMode);
+  };
+
+  const showDatepicker = () => {
+    showMode('date');
+    
+  };
+
+
+  // const showTimepicker = () => {
+  //   showMode('time');
+  // };
+
   const navigation = useNavigation();
-  // const [date, setDate] = useState(new Date(1598051730000));
-  // const [mode, setMode] = useState('date');
-  // const [show, setShow] = useState(false);
-
-  // const onChange = (event, selectedDate) => {
-  //   const currentDate = selectedDate || date;
-  //   setShow(Platform.OS === 'ios');
-  //   setDate(currentDate);
-  // };
-
-  // const showMode = (currentMode) => {
-  //   setShow(true);
-  //   setMode(currentMode);
-  // };
-
-  // const showDatepicker = () => {
-  //   showMode('date');
-  // };
-
 
     return (
         <>
@@ -57,9 +78,31 @@ const FormTask = () => {
             </View>
             <View style={styles.formItem}>
             <TextInput 
-            style={tw `pl-3 pt-3 pb-3 bg-gray-200 rounded ml-4 h-12 w-80`}
+              style={tw `pl-3 pt-3 pb-3 bg-gray-200 rounded ml-4 h-12 w-80`}
               placeholder= "2021-02-28"
             />
+            <TouchableOpacity
+            style={{position: 'absolute', marginLeft: 300}}
+            onPress={showDatepicker}
+            >
+             <View>
+                <Icon 
+                  type="MaterialIcons"
+                  name="keyboard-arrow-down"
+                />
+             </View>
+            </TouchableOpacity>
+            {show && (
+        <DateTimePicker
+          testID="dateTimePicker"
+          value={date}
+          mode={mode}
+          is24Hour={true}
+          display="default"
+          onChange={onChange}
+          style={tw.style({width: 320, backgroundColor: "#E5E7EB", position: 'absolute', marginLeft:16})} 
+        />
+          )}
             </View>
             <View style={styles.formRowTwo}>
             <Text style={styles.formLabel}>Start Time</Text>
@@ -67,8 +110,10 @@ const FormTask = () => {
             </View>
             <View style={styles.formRowTwo}>
             <TextInput 
+            value={time}
+            onChangeText={setTime}
             style={tw.style('pl-2 pb-3 pt-3 bg-gray-200 ml-4 h-12 rounded w-36',{marginTop: -20})}
-              placeholder= "11:00 Am"
+            placeholder= "11:00 Am"
             />
             <TextInput 
             style={tw.style('pl-3 pb-3 pt-3 bg-gray-200 rounded h-12 w-36',{marginTop: -20, marginRight: 20})}
@@ -79,9 +124,29 @@ const FormTask = () => {
             <Text style={styles.formLabel}>Remind</Text>
             </View>
             <View style={styles.formItem}>
-            <TextInput 
+            {/* <TextInput 
             style={tw `pl-3 pt-3 pb-3 bg-gray-200 rounded ml-4 h-12 w-80`}
               placeholder= "10 minutes early"
+            /> */}
+            <DropDownPicker
+              style={tw.style('bg-gray-200 rounded ml-4 w-80', {borderWidth: 0})}
+              labelStyle={{
+              color: "#868686" 
+              }}
+              dropDownDirection="TOP"
+              containerStyle={tw `w-80`}
+              placeholder="10 minutes early"
+              placeholderStyle={{
+                color: "#868686",
+                opacity: 0.5
+              }}
+              dropDownContainerStyle={{borderWidth: 0, backgroundColor: "#E5E7EB" }}
+              open={openRemind}
+              value={valueRemind}
+              items={reminditems}
+              setOpen={setRemindOpen}
+              setValue={setValueRemind}
+              setItems={setRemindiItems}
             />
             </View>
             <View style={styles.formRowTwo}>
@@ -97,6 +162,13 @@ const FormTask = () => {
               labelStyle={{
               color: "#868686" 
               }}
+              containerStyle={tw `w-80`}
+              placeholder="Weekly"
+              placeholderStyle={{
+                color: "#868686",
+                opacity: 0.5
+              }}
+              dropDownContainerStyle={{borderWidth: 0, backgroundColor: "#E5E7EB" }}
               open={open}
               value={value}
               items={items}
